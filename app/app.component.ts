3fe8feb34a7561a8ff43c08c977ad9e159659bc9
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-10-31 17:59:19 -0200

    Subindo Obervable

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+ import { Http } from '@angular/http';
 
 @Component({
     selector: 'my-app',
+    viewProviders: [],
     template: '<h1>Hello World Angular</h1>'
 })
 export class AppComponent{
-
+    constructor (http: Http){
+        console.log(http.get('https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1')
+        .subscribe(res => res.json()));      
+      }
 }
