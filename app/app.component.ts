428535f848bedc7b257f0b3a412315abdb063ffe
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-11-01 09:56:52 -0200

    novas alterações no componente

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -7,10 +7,18 @@ import 'rxjs/add/operator/map';
     viewProviders: [],
     template: '<h1>Hello World Angular</h1>'
 })
-export class AppComponent{
-    constructor (http: Http){
-        console.log(http.get('https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1').map(res => res.json()));    
+export class AppComponent implements OnInit{
+    constructor (private http: Http){
+       // console.log(http.get('https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1').map(res => res.json()));    
       }
+
+
+      ngOnInit(): void {
+        this.http.get('https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1').subscribe(data => {
+          console.log(data);
+        });
+      }
+      
 
 /*  falta terminar
 
